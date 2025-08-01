776416659af094b6b7b0b7a6545c6574cd899094
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2025-08-01 15:39:03 +0500

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+import SearchBar from "./components/SearchBar";
+import FeaturedBook from "./components/FeaturedBook";
+import BookCard from "./components/BookCard";
+import ReviewSection from "./components/ReviewSection";
 
 const App = () => {
-  return (
-    <div>
-      wd
-    </div>
-  )
-}
+  const books = [
+    {
+      title: "1984",
+      rating: 4.5,
+      description: "A dystopian novel by George Orwell."
+    },
+    {
+      title: "To Kill a Mockingbird",
+      rating: 4.8,
+      description: "A story of justice and racial prejudice."
+    },
+  ];
 
-export default App
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Navbar />
+      <SearchBar />
+      <FeaturedBook />
+      <div className="flex flex-wrap justify-center gap-6 mt-10 px-4">
+        {books.map((book, idx) => (
+          <BookCard key={idx} {...book} />
+        ))}
+      </div>
+      <ReviewSection />
+    </div>
+  );
+};
+
+export default App;
